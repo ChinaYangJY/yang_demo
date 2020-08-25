db5c2fb4daa25da18b8633c5fe397d1b50998d9a
Yang <chinayangjy@163.com>
2020-08-25 15:24:32 +0800

    init

---
+++ src/main/java/com/china/yang_demo/YangDemoApplication.java
@@ -0,0 +1,13 @@
+package com.china.yang_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangDemoApplication.class, args);
+    }
+
+}
