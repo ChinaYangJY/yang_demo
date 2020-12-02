a24ba67fdee8d936a78ccd4a7a19dc3dbdc36ac3
Yang <chinayangjy@163.com>
2020-12-02 14:51:39 +0800

    websocket init

---
+++ src/main/java/com/china/yang_demo/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.china.yang_demo.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@SpringBootConfiguration
+public class WebSocketConfig {
+    /**
+     * ServerEndpointExporter 作用
+     * <p>
+     * 这个Bean会自动注册使用@ServerEndpoint注解声明的websocket endpoint
+     *
+     * @return
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
--- src/main/java/com/china/yang_demo/redis/controller/TestController.java
+++ src/main/java/com/china/yang_demo/redis/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.china.yang_demo.redis.controller;
 
+import com.china.yang_demo.utils.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +14,17 @@ public class TestController {
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @GetMapping("/test/{id}")
-    public void test(@PathVariable String id){
-        System.out.println("test~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:"+id);
-        redisTemplate.opsForValue().set("test","这是一个样本数据");
+    public void test(@PathVariable String id) {
+        System.out.println("test~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:" + id);
+        redisTemplate.opsForValue().set("test", "这是一个样本数据");
         Object test = redisTemplate.opsForValue().get("test");
         System.out.println(test);
+        redisUtil.set("testB","B样本数据");
+        Object testB = redisUtil.get("testB");
+        System.out.println(testB);
     }
 }
